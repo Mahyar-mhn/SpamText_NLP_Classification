7c189724db71e4baab81cc7ead6bdc3c39c0edee
Mahyar_Mohammadian <mhn.mahyarmohammadian1383@gmail.com>
2024-06-02 01:38:54 +0330

    replace span and ham with 1 and 0 and count them

--- SpamText.py
+++ SpamText.py
@@ -8,4 +8,12 @@ from nltk.corpus import stopwords
 
 # loading data set
 df = pd.read_csv("SPAM text message 20170820 - Data.csv")
-print(df.head())
+# print(df.head())
+
+# set ham and spam into 0 and 1
+df["Category"].replace({1: "spam", 0: "ham"}, inplace=True)
+
+# gain insight from data
+data = {'Category': ['spam', 'ham'], 'number': [len(df.loc[df['Category'] == 1]), len(df.loc[df['Category'] == 0])]}
+df_count = pd.DataFrame(data, columns=['Category', 'number'])
+print(df_count.head())
